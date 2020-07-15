990c1fd969f44009aa91145b78e5216aee201675
start-50up <60340469+start-50up@users.noreply.github.com>
2020-07-16 04:33:39 +0700

    1 create components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import "../styles.css";
 
-const NavItem = props => <li>{props.text}</li>;
+const NavItem = props => (
+  <li>
+    <a
+      href={props.href}
+      className="text-sm font-bold text-gray-700 px-2 py-1 hover:bg-gray-300 rounded"
+    >
+      {props.text}
+    </a>
+  </li>
+);
+
+const Button = props => (
+  <Button
+    type={props.type}
+    className="bg-red-400 hover:bg-red-500 transition-colors duration-300"
+  />
+);
 
 export default () => (
   <div>
@@ -9,9 +25,9 @@ export default () => (
         <span>Notion</span>
       </div>
       <ul className="flex space-x-2">
-        <NavItem text="Product" />
-        <NavItem text="Download" />
-        <NavItem text="Remote work" />
+        <NavItem href="/product" text="Product" />
+        <NavItem href="/download" text="Download" />
+        <NavItem href="/remote-work" text="Remote work" />
       </ul>
     </nav>
   </div>
